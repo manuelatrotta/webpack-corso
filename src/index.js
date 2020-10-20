5dfe39290b9a1dbf0587614d857e07e99ae6ce2e
manuelatrotta <manuelat88@hotmail.it>
2020-10-20 09:45:45 +0200

    collegamento server

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import 'bootstrap';
+
+import './styles/index.scss';
+
+import './images/webpack.svg';
+
+console.log("hello");
---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    contentBase: 'dist/', // Relative directory for base of server
+    publicPath: '/', // Live-reload
+    inline: true,
+    port: process.env.PORT || 3000, // Port Number
+    host: 'localhost', // Change to '0.0.0.0' for external facing server
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            // Adds CSS to the DOM by injecting a `<style>` tag
+            loader: 'style-loader'
+          },
+          {
+            // Interprets `@import` and `url()` like `import/require()` and will resolve them
+            loader: 'css-loader'
+          },
+          {
+            // Loads a SASS/SCSS file and compiles it to CSS
+            loader: 'sass-loader'
+          }
+        ]
+      },
+      {
+        test: /\.(js)$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+            },
+          },
+          'image-webpack-loader',
+        ],
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html',
+    }),
+  ],
+};
